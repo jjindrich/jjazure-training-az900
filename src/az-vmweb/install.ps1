9f2eb1e050312ffafb010c4dbd8436ace9c06e7f
Jaroslav Jindrich <jajindri@microsoft.com>
2020-05-21 14:17:23 +0200

    fix ps1

--- src/az-vmweb/install.ps1
+++ src/az-vmweb/install.ps1
@@ -9,4 +9,4 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
 # Install ASP.NET Core Runtime Windows Hosting Bundle Installer
 # https://dotnet.microsoft.com/download/dotnet-core
 Invoke-WebRequest "https://download.visualstudio.microsoft.com/download/pr/ba001109-03c6-45ef-832c-c4dbfdb36e00/e3413f9e47e13f1e4b1b9cf2998bc613/dotnet-hosting-2.2.8-win.exe" -OutFile dotnet-hosting-2.2.8-win.exe
-dotnet-hosting-2.2.8-win.exe /silent
+.\dotnet-hosting-2.2.8-win.exe /silent
